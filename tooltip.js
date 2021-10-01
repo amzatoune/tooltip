629f19f90a157c27d22be4364bf558b3f03b645c
amzatoune <wanderertimeless@gmail.com>
2021-10-02 00:10:47 +0200

    first commit

---
+++ tooltip.js
@@ -0,0 +1,113 @@
+$('.email').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-paper-plane').addClass("next");
+    } else {
+      $('.icon-paper-plane').removeClass("next");
+    }
+  }
+);
+
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
+
+$('.next-button.email').click(
+  function(){
+    console.log("Something");
+    $('.email-section').addClass("fold-up");
+    $('.username-section').removeClass("folded");
+  }
+);
+
+$('.username').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-username').addClass("next");
+    } else {
+      $('.icon-username').removeClass("next");
+    }
+  }
+);
+
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
+
+$('.next-button.username').click(
+  function(){
+    console.log("Something");
+    $('.username-section').addClass("fold-up");
+    $('.birthdate-section').removeClass("folded");
+  }
+);
+
+$('.birthdate').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-birthdaycake').addClass("next");
+    } else {
+      $('.icon-birthdaycake').removeClass("next");
+    }
+  }
+);
+
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
+
+$('.next-button.birthdate').click(
+  function(){
+    console.log("Something");
+    $('.birthdate-section').addClass("fold-up");
+    $('.password-section').removeClass("folded");
+  }
+);
+
+$('.password').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-lock').addClass("next");
+    } else {
+      $('.icon-lock').removeClass("next");
+    }
+  }
+);
+
+$('.next-button').hover(
+  function(){
+    $(this).css('cursor', 'pointer');
+  }
+);
+
+$('.next-button.password').click(
+  function(){
+    console.log("Something");
+    $('.password-section').addClass("fold-up");
+    $('.repeat-password-section').removeClass("folded");
+  }
+);
+
+$('.repeat-password').on("change keyup paste",
+  function(){
+    if($(this).val()){
+      $('.icon-repeat-lock').addClass("next");
+    } else {
+      $('.icon-repeat-lock').removeClass("next");
+    }
+  }
+);
+
+$('.next-button.repeat-password').click(
+  function(){
+    console.log("Something");
+    $('.repeat-password-section').addClass("fold-up");
+    $('.success').css("marginTop", 0);
+  }
+);
